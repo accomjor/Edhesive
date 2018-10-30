091df9f160c0a4b9821eb6fa7b2ecc599a59dd79
accomjor <jordanaccomando@gmail.com>
2018-10-30 11:31:44 -0700

    10/30 Code

--- Edhesive/src/main/Main.java
+++ Edhesive/src/main/Main.java
@@ -8,13 +8,13 @@ import java.lang.Character;
 import java.lang.StringIndexOutOfBoundsException;
 import java.lang.IndexOutOfBoundsException;
 
-public class Main{
+public class Main {
     
     
     /*
     
     Edhesive class normal template for all purposes.
-    version 1.3
+    version 1.4
     
     */
     
@@ -31,119 +31,12 @@ public class Main{
     public static void main(String args[]){
         
         // Code goes here..
-        String input;
-        Scanner scan = new Scanner(System.in);
-        say("Welcome to the Methods Sampler Platter. Please enter a String to duplicate.");
-        input = scan.nextLine();
-        input = duplicate(input);
-        say("The duplicated String is: " + input);
-        say("Next, please enter a String to check for Edhesive Palindromes.");
-        input = scan.nextLine();
-        if(isEdhesivePalindrome(input)){
-            say("Nice, you found an Edhesive Palindrome!");
-        } else {
-            say("Too bad, that isn't an Edhesive Palindrome.");
-        }
-        say("Almost done! Please enter a number to scramble.");
-        d = scan.nextDouble();
-        d = numberScramble(d);
-        say("The scrambled number is: " + d);
-        
+
     }
     
     
     /***************************************************************************************************************/
     
-    public static boolean isEdhesivePalindrome(String str){
-        String s = "";
-        String before = "";
-        String after = "";
-        int length = str.length();
-        int bc = 0;
-        int ac = 0;
-        int charVal = 0;
-        str = str.replace("0", "o");
-        str = str.replace("3", "e");
-        str = str.replace("4", "a");
-        str = str.toLowerCase();
-        if(length > 3 && length < 15){
-            if(length % 2 == 0){
-                for(i = 0; i < length/2; i++){
-                    charVal = ((int) str.charAt(i));
-                    bc += charVal;
-                    // debug
-                    //say(str.charAt(i) + " b " + ((int) str.charAt(i)) + " " + bc);
-                }
-                for(i = length/2; i < length; i++){
-                    ac += (int) str.charAt(i);
-                    // debug
-                    //say(str.charAt(i) + " a " + ((int) str.charAt(i)) + " " + ac);
-                }
-                // debug 
-                //say("bc: " + bc);
-                //say("ac: " + ac);
-            } else {
-                for(i = 0; i < length/2; i++){
-                    charVal = ((int) str.charAt(i));
-                    bc += charVal;
-                    // debug
-                    //say(str.charAt(i) + " b " + ((int) str.charAt(i)) + " " + bc);
-                }
-                for(i = length/2 + 1; i < length; i++){
-                    ac += (int) str.charAt(i);
-                    // debug
-                    //say(str.charAt(i) + " a " + ((int) str.charAt(i)) + " " + ac);
-                }
-                // debug 
-                //say("bc: " + bc);
-                //say("ac: " + ac);
-            }
-            if(bc == ac){
-                return true;
-            }
-            return false;
-        }
-        return false;
-    }
-    
-    
-    public static String duplicate(String str){
-        String s = "";
-        boolean flag = false;
-        int x = 0;
-        int length = str.length();
-        
-        // check if length is even
-        if(length % 2 == 0){ flag = true; }
-        
-        // if even else odd
-        if(flag){
-            for(i = 0; i < length; i++){
-                for(x = 0; x < length*2; x++){
-                    s = s + str.charAt(i);
-                }
-            }
-        } else {
-            for(i = 0; i < length; i++){
-                for(x = 0; x < length; x++){
-                    s = s + str.charAt(i);
-                }
-            }
-        }
-        
-        return s;
-    }
-    
-    public static double numberScramble(double num){
-    	if(num < 0){
-    		return 0.0;
-    	} else {
-    		num += 5;
-    		num /= 2;
-    		num = Math.pow(num, 0.5);
-    		return num;
-    	}
-    }
     
     // print a message to the console on a new line.
     // Updated: Version 1.0
